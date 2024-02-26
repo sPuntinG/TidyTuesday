997b369c8d23011c3e37f571035d8c13c17b3430
sPuntinG <giulia.puntin@gmail.com>
2024-02-26 16:50:19 +0100

    added text and png

--- 2022_12/babynames_perc_title.R
+++ 2022_12/babynames_perc_title.R
@@ -16,61 +16,26 @@ library(here)
 
 babynames <- read_csv("./babynames.csv")
 
-
-# # Right and left keys
-# leftk <- c("Q", "W", "E", "R", "T", "A", "S", "D", "F", "G", "Z", "X", "C", "V", "B")
-# rightk1 <- c("Y", "U", "I", "O", "P", "H", "J", "K", "L", "N", "M")
-# rightk2 <- c("Y", "U", "I", "O", "P", "H", "J", "K", "L", "N", "M", "B") # includes B
-
 babynames$name %>% n_distinct() # 97'310 (!!!)
 names10 <- babynames$name %>% unique() %>% .[1:10] # small set for now as it's a huge data set ... !
 
 
 # Count right key letters in each name ---------------------------------
 
-# # str_count() works on single word
-# stringr::str_count(names10[1], fixed(rightk1, ignore_case = T)) # 1 0 0 0 0 0 0 0 0 0 1   this can simply be summed up with sum() = 2
-# stringr::str_count(names10[1], fixed(rightk1, ignore_case = T)) %>% sum() # 2 which is what I need in the end
-# 
-# # But does not work on vectors
-# stringr::str_count(names10[1:2], fixed(rightk1, ignore_case = T)) # Error in `stringr::str_count()`:
-#                                                                  # ! Can't recycle `string` (size 2) to match `pattern` (size 11).
-
 # Make function to iterate
-count_right1 <- function(x) {
-  rightk1 <- c("Y", "U", "I", "O", "P", "H", "J", "K", "L", "N", "M")
-  a <- stringr::str_count(x, fixed(rightk1, ignore_case = T))
+count_right <- function(x) {
+  rightk <- c("Y", "U", "I", "O", "P", "H", "J", "K", "L", "N", "M")
+  a <- stringr::str_count(x, fixed(rightk, ignore_case = T))
   a <- sum(a)
   return(a)
 }
 
-count_right2 <- function(x) {
-  rightk2 <- c("Y", "U", "I", "O", "P", "H", "J", "K", "L", "N", "M", "B")
-  # rightk <- paste(rightk, collapse = '|')
-  a <- stringr::str_count(x, fixed(rightk2, ignore_case = T))
-  a <- sum(a)
-  return(a)
-}
 
-count_right1("mario") # 3
+count_right("mario") # 3
 # count_right1(names10) # as expected doesn't work on vectorized argument
 
 
 # Use map_dbl to loop count right function over vector of names -----------------------
-
-# map_dbl(names10, count_right1) # so that the output is a numeric vector 
-# 
-# tibble10 <- tibble(name = names10, 
-#                    tot_letters = nchar(name),
-#                    right_letters = map_dbl(names10, count_right1),
-#                    left_letters = tot_letters - right_letters,
-#                    ratio = right_letters/left_letters,
-#                    predominance = case_when(
-#                      ratio == 1 ~ "none",
-#                      ratio > 1 ~ "right",
-#                      ratio < 1 ~ "left"
-#                      )
-#                    )
 
 
 # Apply to full list of names 
@@ -79,24 +44,15 @@ all_names <- babynames$name %>% unique()
 
 tibble_all <- tibble(name = all_names, 
                      tot_letters = nchar(name),
-                     right_letters1 = map_dbl(all_names, count_right1),
-                     right_letters2 = map_dbl(all_names, count_right2),
-                     left_letters1 = tot_letters - right_letters1,
-                     left_letters2 = tot_letters - right_letters2,
-                     ratio1 = right_letters1/left_letters1,
-                     ratio2 = right_letters2/left_letters2,
-                     predominance1 = case_when(
-                       ratio1 == 1 ~ "none",
-                       ratio1 > 1 ~ "right",
-                       ratio1 < 1 ~ "left"
-                     ),
-                     predominance2 = case_when(
-                       ratio2 == 1 ~ "none",
-                       ratio2 > 1 ~ "right",
-                       ratio2 < 1 ~ "left"
-                     ),
-                     perc_right1 = right_letters1/tot_letters * 100,
-                     perc_right2 = right_letters2/tot_letters * 100
+                     right_letters = map_dbl(all_names, count_right),
+                     left_letters = tot_letters - right_letters,
+                     ratio = right_letters/left_letters,
+                     predominance = case_when(
+                       ratio == 1 ~ "none",
+                       ratio > 1 ~ "right",
+                       ratio < 1 ~ "left"
+                     ),           
+                     perc_right = right_letters/tot_letters * 100
 )
    
 
@@ -111,33 +67,25 @@ babynames_rl <- left_join(babynames, tibble_all, by = "name")
 "#FFFFFF"
 "#0A3161"
 
-# midcolor
-"#5F2552"
-
-# Lighter versions
-"#F7D0EF"
-"#F7EBF5"
-"#FFDEF9"
 
 
-# Fonts ------------------
+
+# Fonts OLD ------------------
 windowsFonts()
 
 # install.packages("extrafont")
 library(extrafont)
-# font_import() # to run only when new fonts added to the system
+# font_import() # to run only when new fonts added to the system?
 loadfonts(device = "win")
+
+extrafont::fonttable() %>% view()
 
 windowsFonts()
 
 
 # Plot: weighted mean % of right vs left key letters in baby names -----------------------------
 
-# Now plot average percentage of right key letters in names (per year)
-# NOTE: old version was not normalized for the frequency of the names! 
-#       perc_right should be multiplied by n and then averaged (divided by total n)
-
-library(ggtext) # for element_markdown()
+# Plot average percentage of right key letters in names (per year)
 
 # New version with normalization by n
 babynames_rl_toplot <- babynames_rl %>% 
@@ -146,15 +94,173 @@ babynames_rl_toplot <- babynames_rl %>%
   mutate(n_tot = sum(n)) %>% 
   ungroup() %>% 
   mutate(
-    perc_right1_n = perc_right1 * n
+    perc_right_n = perc_right * n
   ) %>% 
   # select(year, n, n_tot, perc_right1, perc_right1_n) %>% #view() name, 
   group_by(year) %>%
-  reframe(perc_right1_n_mean = sum(perc_right1_n) / n_tot) %>% #view()
+  reframe(perc_right_n_mean = sum(perc_right_n) / n_tot) %>% #view()
   unique()
 
 
-plot1 <- ggplot(babynames_rl_toplot, aes(x = year, y = perc_right1_n_mean)) +
+
+# NEW VERSION OF PLOT building:
+#  p1 = plot
+#  p2 = plot + text annotations 
+
+
+
+# # Fonts NEW with {showtext} --------------
+# # install.packages("showtext")
+# library(showtext)
+# 
+# # Install font from google fonts
+# sysfonts::font_add_google('Special Elite', 'special_elite')
+# sysfonts::font_add_google('Gloria Hallelujah', 'gloria_halleluja')
+# sysfonts::font_add_google('Poppins', 'poppins')
+# 
+# 
+# # font_add(family = "<family_name>", regular = "/path/to/font/file")
+# 
+# showtext::showtext_auto(F)
+
+
+
+library(ggtext) # for element_markdown() and geom_textbox()
+
+# Main plot
+p1 <- ggplot(babynames_rl_toplot, aes(x = year, y = perc_right_n_mean)) +
+  geom_hline(yintercept = c(45, 50), color = "white", linewidth = 1.25) +
+  # geom_hline(yintercept =  seq(35, 47, 1), color = "white", linewidth = 0.7) +
+  # geom_hline(yintercept =  seq(47, 51, 1), color = "white", linewidth = 0.3) +
+  geom_hline(yintercept =  seq(44, 53, 1), color = "white", alpha = 0.9,
+             linewidth = seq(0.9, 0, -0.1)) +
+  geom_area(fill = "#B31942") +
+  geom_line(color = "white", linewidth = 1) +
+  # geom_hline(yintercept = 40, color = "white") + # c(40, 45, 50, 55) linetype = "dashed",  , alpha = 0.3
+  scale_y_continuous(expand = c(0,0), 
+                     breaks = c(45, 50),
+                     labels = function(x) paste0(x, " %")) + 
+  scale_x_continuous(expand = c(0,0),
+                     breaks = seq(1890, 2010, 20)) +
+  coord_cartesian(ylim = c(38, 70)) + # coord_cartesian(ylim = c(40, 55)) + coord_cartesian(ylim = c(40, 70)) +
+  theme(
+    text = element_text(colour = "white", family = "Poppins"),
+    plot.background = element_rect(fill = "#0A3161", color = NA),
+    panel.background = element_rect(fill = "#0A3161", color = NA),
+    # plot.margin = margin(t = 5, r = 0, b = 0, l = 0, unit = "cm"), #margin(rep(1, 4)),
+    panel.grid.major = element_blank(), 
+    panel.grid.minor = element_blank(),
+    axis.ticks = element_blank(),
+    axis.title = element_blank(),
+    axis.text = element_text(colour = "white",size = 12),
+    axis.text.y = element_text(vjust = -0.7, # -1
+                               margin = margin(l = 15, r = -45)),  
+    axis.text.x = element_text(margin = margin(t = -20, b = 5))
+  )
+
+p1
+
+
+# Add textbox for description
+
+text_title <- tibble(
+  year = c(1885),
+  y = c(65), # c(67.5),
+  text = c(
+    # "American <span style='font-family:Gloria Hallelujah'>baby names</span> and the <span style='font-family:Special Elite'>QWERTY effect</span>:<br> is tech steering naming preferences?"
+    "<span style='font-size:18pt'>American baby names and the QWERTY effect:<br> is tech steering naming preferences?</span><br><br>
+    "
+    # "American <span style='font-family:gloria_halleluja'>baby names</span> and the <span style='font-family:special_elite'>QWERTY effect</span>: is tech steering naming preferences?"
+    )
+)
+
+text_descr <- tibble(
+  year = c(1885),
+  y = c(59.5), # 62 
+  text = c(
+    "The **‘QWERTY effect’** describes a phenomenon where words containing more letters from the right side of the keyboard are perceived more positively (Jasmin and Casasanto 2012).<br><br> 
+    This influence seems to extend to the selection of baby names in the U.S., with names rich in right-side-key letters gaining popularity since the 1990s, coinciding with the widespread adoption of QWERTY keyboards as computers and the internet became household staples (Casasanto et al. 2014).<br><br>
+This plot shows the **average percentage** of **<span style='color:red'>right</span>**-side-key letters in names given to **newborns** in the **U.S.** each year **from 1880 to 2017**.<br><br>
+Essentially, the plotted line (%) goes up as names containing a higher percentage of right-side-key letters rise in popularity.
+"
+  )
+)
+
+
+# Add image
+img <- png::readPNG("qwertyLR.png")
+raster <- grid::rasterGrob(img, interpolate = TRUE)
+
+
+
+p1 +
+  annotation_custom(raster, x = 1960, xmax = 2010, y = 69, ymax = 66) + # x = 1950, xmax = 2010, y = 69, ymax = 66
+  ggtext::geom_textbox(
+    data = text_title,
+    mapping = aes(x = year,
+                  y = y,
+                  label = text),
+    inherit.aes = FALSE,
+    hjust = 0,
+    size = 5,
+    width = unit(15, "cm"),
+    family = "Special Elite", # family = "Poppins", # works     # family = "Gloria Hallelujah", # works
+    fill = NA,
+    box.color = NA,
+    color = "white" # text color
+  ) +
+  ggtext::geom_textbox(
+    data = text_descr,
+    mapping = aes(x = year,
+                  y = y,
+                  label = text),
+    inherit.aes = FALSE,
+    hjust = 0,
+    size = 4,
+    width = unit(15, "cm"),
+    family = "Poppins", # works
+    fill = NA,
+    box.color = NA,
+    color = "white" # text color
+  ) +
+  geom_textbox(data = data.frame(x = 1952, 
+                                 y = 42.5160724790146, 
+                              label = "In **1990** computers and internt start to become popular household items"),
+            mapping = aes(x = x, y = y, label = label),
+            family = "Poppins", # "Cutive"
+            fill = NA,
+            box.color = NA,
+            color = "white",
+            width = unit(9, "cm"),
+            valign = 1,
+            # hjust = 0.5,
+            inherit.aes = FALSE) +
+  geom_curve(data = data.frame(x = 1980, y = 42.5160724790146, xend = 1990, yend = 44.2951979644509),
+             mapping = aes(x = x, y = y, xend = xend, yend = yend),
+             angle = 90,
+             color = "white",
+             arrow = arrow(23L, unit(0.1, "inches"), "last", "closed"),
+             inherit.aes = FALSE)
+ # add "caption" "#TidyTuesday 2022_12 | Data from: XXXX | Viz by **Giulia Puntin**"
+
+
+
+# Print png file
+ggsave("./try_again.png",
+       dpi = 330,
+       units = "cm", 
+       width = 17, height = 24 # A4 format 21 x 29.7
+)
+
+
+
+
+
+
+# OLD xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+
+
+plot1 <- ggplot(babynames_rl_toplot, aes(x = year, y = perc_right_n_mean)) +
   geom_hline(yintercept = c(45, 50), color = "white", linewidth = 1.25) +
   # geom_hline(yintercept =  seq(35, 47, 1), color = "white", linewidth = 0.7) +
   # geom_hline(yintercept =  seq(47, 51, 1), color = "white", linewidth = 0.3) +
@@ -173,14 +279,25 @@ plot1 <- ggplot(babynames_rl_toplot, aes(x = year, y = perc_right1_n_mean)) +
 
 # Figure out if annotate() or geom_text() better for title and subtitle etc (which one easier?)
 # Then use {ggannotate} for arrows
+
+# font: Gloria Hallelujah
+md_title <- "American <span style='font-family: Calibri;'>baby names</span> and the '<span style='font-family: Cutive;'>QWERTY effect</span>': is tech steering naming preferences?"
+
+md_subtitle <- "American baby names and the '**QWERTY effect**': is tech steering naming preferences?
+
+The ‘**QWERTY effect**’ describes a phenomenon where words containing more letters from the right side of the keyboard are perceived more positively (REF1). This influence seems to extend to the selection of baby names in the U.S., with names rich in right-side-key letters gaining popularity since the 1990s, coinciding with the widespread adoption of QWERTY keyboards as computers and the internet became household staples (REF2).
+
+This plot shows the average percentage of right-side-key letters in names given to newborns in the U.S. each year from 1880 to 2017.
+
+Essentially, the plotted line (percentage) goes up as names containing a higher percentage of right-side-key letters rise in popularity. 
+
+While this analysis does not offer a definitive answer regarding the influence of technology on semantics, it presents an intriguing aspect worthy of consideration."
+
+
+
 plot1 + 
-  labs(title = "This is a text in <span style='font-family: Cutive; color: red'>QWERTY effect</span> ",
-       subtitle = "write the rest of the text here ...") +
-  # geom_text(data = data.frame(x = 1889.54553535584, y = 67.9509789002066, label = "The 'QWERTY effect' on U.S. baby names"),
-  #           mapping = aes(x = x, y = y, label = label),
-  #           family = "Abril Fatface",
-  #           # fontface = 2, 
-  #           size = 4.59, hjust = 0L, colour = "white", inherit.aes = FALSE) + # 
+  labs(title = md_title,
+       subtitle = md_subtitle) +
   geom_text(data = data.frame(x = 1930, y = 42.5160724790146, label = "In 1990 computers start to become popular: 
 they are found in ~19% of households"),
              mapping = aes(x = x, y = y, label = label),
@@ -215,6 +332,9 @@ they are found in ~19% of households"),
   )
 
 
+# Add PNG: see https://stackoverflow.com/questions/74878716/adding-image-in-title-spaces-using-ggplot2
+
+
 
 # Annotate with {gganotate} ---------------------------------
 
@@ -227,7 +347,7 @@ they are found in ~19% of households"),
 
 
 # Print png file
-ggsave("./try3.png",
+ggsave("./try4.png",
        dpi = 330,
        units = "cm", 
        width = 17, height = 24 # A4 format 21 x 29.7
